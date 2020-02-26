697ac108a7fef5a98283883cd8fa6143b19c74e7
korytovaf <kaf1@mail.ru>
2020-02-26 23:10:17 +0500

    Добавил плавную прокрутку + поправил бургер меню

--- script/burger-menu.js
+++ script/burger-menu.js
@@ -1,15 +1,27 @@
 $(function () {
 
 
-	//меню бургер
-	$('.burger, .menu a').on('click', function(e){
+	$('.burger').on('click', function(e){
 		e.preventDefault();
 
-		$('.header__nav').fadeToggle(400, 'linear');
-		
+		$('.header__nav').slideToggle(100, 'linear');
 		$('.burger-btn').toggleClass('burger-btn-click');
 	});
 	
+	
+	$('.header__nav_menu_list, .scroll-offset').on('click', function (e) {
+		e.preventDefault();
+	
+		let href = $(this).attr('href');
+		let offset = $(href).offset().top;
+
+		$('body, html').animate({
+			scrollTop: offset
+		}, 600);
+
+	});
+	
+
 
 
 });

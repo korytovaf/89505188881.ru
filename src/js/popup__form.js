bb01b61ebf2903566a59c0db9556be72288844db
korytovaf <61353426+korytovaf@users.noreply.github.com>
2020-03-22 20:54:49 +0300

    v.2.5

--- src/js/popup__form.js
+++ src/js/popup__form.js
@@ -4,29 +4,29 @@ $(function () {
 	// для вызова Popup окна, кнопкам прописать класс .order
 	$('.order').on('click', function(e){
 		e.preventDefault()
-		$('.form').fadeIn(400,
+		$('.popup__background').fadeIn(150,
             function(){
                 $('.popup') 
                     .css('display', 'block')
-                    .animate({opacity: 1, left: '50%'}, 400);
+                    .animate({opacity: 1, left: '50%'}, 150);
         });
 		$('html').addClass('popup__html_fixed');
 		$('body').addClass('popup__body_fixed');
 		$('#name').focus();
 	});
 
-	$('.popup__close, .form').on('click', function(){
+	$('.popup__close, .popup__background').on('click', function(){
 				
-		$('.popup').animate({opacity: 0, left: '45%'}, 400, function(){
+		$('.popup').animate({opacity: 0, left: '45%'}, 150, function(){
 			$(this).css('display', 'none');
-			$('.form').fadeOut(400);
+			$('.popup__background').fadeOut(150);
 		});
 
 		$('html').removeClass('popup__html_fixed');
 		$('body').removeClass('popup__body_fixed');
 	});
 
-	$('input[type="tel"]').inputmask({"mask": "+7(999)999-99-99"});
+	$('input[type="tel"]').inputmask({"mask": "+7 999 999 9999"});
 
 	//Отправка формы через mailer
 	$('form').validate({
@@ -52,12 +52,12 @@ $(function () {
 			}).done(function() {
 				$(this).find("input").val("");
 
-				$('.popup').fadeOut(400);
+				$('.popup').fadeOut(150);
 				
 
                 $('.popup__alert') 
                     .css('display', 'block')
-                    .animate({opacity: 1, left: '50%'}, 400);
+                    .animate({opacity: 1, left: '50%'}, 150);
         		
 
 				$('form').trigger("reset");
@@ -71,9 +71,9 @@ $(function () {
 	//Сообщение об успешной отправке формы
 	$('.popup__alert_button').on('click', function(){
 		
-		$('.popup__alert').animate({opacity: 0, left: '45%'}, 400, function(){
+		$('.popup__alert').animate({opacity: 0, left: '45%'}, 150, function(){
 			$(this).css('display', 'none');
-			$('.form').fadeOut(400);
+			$('.popup__background').fadeOut(150);
 		});
 
 		$('html').removeClass('popup__html_fixed');

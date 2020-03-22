70972f73095e3153cf562b32c6755d6c2aab34c8
korytovaf <61353426+korytovaf@users.noreply.github.com>
2020-03-22 21:24:51 +0300

    v.2.6

--- src/js/popup__form.js
+++ src/js/popup__form.js
@@ -54,11 +54,7 @@ $(function () {
 
 				$('.popup').fadeOut(150);
 				
-
-                $('.alert') 
-                    .css('display', 'block')
-                    .animate({opacity: 1, left: '50%'}, 150);
-        		
+    $('.alert').css('display', 'block').animate({opacity: 1, left: '50%'}, 150);
 
 				$('form').trigger("reset");
 			});
